f865d3223cf713895c5ab65e56c99f8f91aec648
unknown <raflisilehu20@gmail.com>
2024-11-26 14:00:36 +0700

    initial commit

---
+++ app/src/main/java/com/nutrigoal/nutrigoal/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.nutrigoal.nutrigoal.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
